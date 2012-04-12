94e129a928d1e718d246e1ae24712f3301fb6ca4
Craig Cook <craigcook@gmail.com>
2012-04-11 17:23:09 -0700

    Moved masthead into its own include so child themes can override it.

--- themes/OneMozilla/header.php
+++ themes/OneMozilla/header.php
@@ -54,7 +54,10 @@
   <?php	wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?> role="document">
+
+<?php $theme_options = onemozilla_get_theme_options(); ?>
+
+<body <?php body_class($theme_options[color_scheme]); ?> role="document">
 <div id="page"><div class="wrap">
   <nav id="nav-access">
     <ul role="navigation">
@@ -66,19 +69,7 @@
     </ul>
   </nav>
 
-	<header id="masthead" role="banner">
-		<hgroup>
-    <?php if ( (is_front_page()) && ($paged < 1) ) : ?>
-      <h1 id="site-title"><?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?></h1>
-    <?php else : ?>
-      <h1 id="site-title"><a href="<?php echo esc_url( home_url('/') ); ?>" rel="home" title="<?php _e('Go to the front page', 'onemozilla'); ?>"><?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?></a></h1>
-    <?php endif; ?>
-    <?php if (get_bloginfo('description','display')) : ?>
-      <h2 id="site-description"><?php echo esc_attr( get_bloginfo('description', 'display') ); ?></h2>
-    <?php endif; ?>
-		</hgroup>
-	  <a href="http://www.mozilla.org/" id="tabzilla">Mozilla</a>
-	</header><!-- #branding -->
+  <?php get_template_part( 'masthead' ); ?>
 
 	<?php wp_nav_menu( array( 'theme_location' => 'primary', 'container' => 'nav', 'container_id' => 'nav-primary', 'fallback_cb' => 'false', ) ); ?>
 
