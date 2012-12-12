f7a37c5a6a01c1c9f97d70007037a5f441eb6369
Craig Cook <craigcook@gmail.com>
2012-12-12 09:32:58 -0800

    Bug 815233 - add Google Analytics

--- themes/OneMozilla/header.php
+++ themes/OneMozilla/header.php
@@ -54,6 +54,17 @@
     else { wp_title(''); echo ' | '; bloginfo('name'); fc_page_number(); }
   ?></title>
 
+  <script type="text/javascript">
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-36116321-4']);
+    _gaq.push(['_trackPageview']);
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+  </script>
+
   <?php if ( is_singular() && get_option( 'thread_comments' ) ) { wp_enqueue_script( 'comment-reply' ); } ?>
   <?php wp_head(); ?>
 </head>
