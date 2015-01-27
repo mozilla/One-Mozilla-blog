616572652247afbc439879f21e955b4e79717bdb
MikkCZ <michal.stanke@mikk.cz>
2015-01-27 12:59:59 +0100

    Update old, "en-US" hardoced and non HTTPS links

--- themes/OneMozilla/masthead.php
+++ themes/OneMozilla/masthead.php
@@ -9,5 +9,5 @@
       <h2 id="site-description"><?php echo esc_attr( get_bloginfo('description', 'display') ); ?></h2>
     <?php endif; ?>
     </hgroup>
-    <a href="http://www.mozilla.org/" id="tabzilla">Mozilla</a>
+    <a href="https://www.mozilla.org/" id="tabzilla">Mozilla</a>
   </header><!-- #masthead -->
