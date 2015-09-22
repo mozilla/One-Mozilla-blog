0d711909c75135f0fe94790799e7faf7b9127a70
MikkCZ <michal.stanke@mikk.cz>
2015-09-22 13:03:59 +0200

    Invalid HTML cleanup (deprecated media projection and default tag roles)

--- themes/OneMozilla/search.php
+++ themes/OneMozilla/search.php
@@ -14,7 +14,7 @@ get_header(); ?>
 
     <?php if (fc_show_posts_nav()) : ?>
     <nav class="nav-paging top">
-      <ul role="navigation">
+      <ul>
         <?php if ( $paged < $wp_query->max_num_pages ) : ?><li class="prev"><?php next_posts_link(__('Older posts','onemozilla')); ?></li><?php endif; ?>
         <?php if ( $paged > 1 ) : ?><li class="next"><?php previous_posts_link(__('Newer posts','onemozilla')); ?></li><?php endif; ?>
       </ul>
@@ -30,7 +30,7 @@ get_header(); ?>
 
     <?php if (fc_show_posts_nav()) : ?>
     <nav class="nav-paging bottom">
-      <ul role="navigation">
+      <ul>
         <?php if ( $paged < $wp_query->max_num_pages ) : ?><li class="prev"><?php next_posts_link(__('Older posts','onemozilla')); ?></li><?php endif; ?>
         <?php if ( $paged > 1 ) : ?><li class="next"><?php previous_posts_link(__('Newer posts','onemozilla')); ?></li><?php endif; ?>
       </ul>
