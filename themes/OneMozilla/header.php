51adacfce03d1bbcaaf7a25d4466465ba00a1cec
Michal Stanke <michal@stanke.cz>
2016-08-14 13:55:14 +0200

    Separate preference for page sharing
    fix https://github.com/mozilla/One-Mozilla-blog/issues/41

--- themes/OneMozilla/header.php
+++ themes/OneMozilla/header.php
@@ -43,7 +43,7 @@
   <link rel="copyright" href="#colophon">
   <link rel="profile" href="http://gmpg.org/xfn/11">
   <link rel="shortcut icon" type="image/ico" href="<?php echo get_template_directory_uri(); ?>/img/favicon.ico">
-  <?php if ( get_option('onemozilla_share_posts') == 1 ) : ?>
+  <?php if ( get_option('onemozilla_share_posts') == 1 || get_option('onemozilla_share_pages') == 1 ) : ?>
   <link rel="stylesheet" type="text/css" media="all" href="<?php echo get_template_directory_uri(); ?>/css/socialshare.css">
   <?php endif; ?>
   <link rel="stylesheet" type="text/css" media="screen" href="<?php bloginfo('stylesheet_url'); ?>">
