d4d3b0d7fe874c5bafad2b8b6dc7ad8fc07df26e
Stephanie Hobson <sjhobson@gmail.com>
2018-08-24 16:24:53 -0700

    Add highlight effect to site title

--- themes/OneMozilla/masthead.php
+++ themes/OneMozilla/masthead.php
@@ -6,17 +6,17 @@
 		<?php wp_nav_menu( array( 'theme_location' => 'top', 'container' => 'nav', 'container_id' => 'nav-top', 'fallback_cb' => 'false', ) ); ?>
 	</div>
 </nav>
-<div class="wrap">
-  <header id="masthead" <?php if (get_header_image()) : ?>class="image"<?php endif; ?>>
+<header id="masthead" <?php if (get_header_image()) : ?>class="image"<?php endif; ?>>
     <hgroup>
-    <?php if ( (is_front_page()) && ($paged < 1) ) : ?>
-      <h1 id="site-title"><span><?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?></span></h1>
-    <?php else : ?>
-      <h1 id="site-title"><span><a href="<?php echo esc_url( home_url('/') ); ?>" rel="home" title="<?php _e('Go to the front page', 'onemozilla'); ?>"><?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?></a></span></h1>
-    <?php endif; ?>
-    <?php if (get_bloginfo('description','display')) : ?>
-      <h2 id="site-description"><span><?php echo esc_attr( get_bloginfo('description', 'display') ); ?></span></h2>
-    <?php endif; ?>
+        <div class="wrap">
+            <?php if ( (is_front_page()) && ($paged < 1) ) : ?>
+              <h1 id="site-title"><span><?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?></span></h1>
+            <?php else : ?>
+              <h1 id="site-title"><span><a href="<?php echo esc_url( home_url('/') ); ?>" rel="home" title="<?php _e('Go to the front page', 'onemozilla'); ?>"><?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?></a></span></h1>
+            <?php endif; ?>
+            <?php if (get_bloginfo('description','display')) : ?>
+              <h2 id="site-description"><span><?php echo esc_attr( get_bloginfo('description', 'display') ); ?></span></h2>
+            <?php endif; ?>
+        </div>
     </hgroup>
-  </header><!-- #masthead -->
-</div>
+</header><!-- #masthead -->
